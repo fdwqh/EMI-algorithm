7ee2c7bbf4253ba119c57eb34c9659fe089883c8
fdwqh <50144812+fdwqh@users.noreply.github.com>
2023-06-17 16:38:26 +0800

    Update Main.m

--- Main.m
+++ Main.m
@@ -1,27 +1,21 @@
 function Main
 clc;
 %% data process
-BaseData=load("200P\ExpNew2-VE.txt");
+BaseData=load("Exp1-VE.txt");
 N=BaseData(1,1); %points number
 M=BaseData(1,2); %edge number
 Edges=BaseData(2:M+1,:);
-Points=load("200P\ExpNew2-P.txt");
+Points=load("Exp1-P.txt");
 Points=Points*1000;
 % [N,M,Edges,Points,Matrix]=generate_network_3d;
 
 %% mutually rigid vertex pairs
 [MP1,cutoff]=IdentifyMutuallyPairs(N,Edges,Points);
-% [MP2,cutoff]=IdentifyMutuallyPairs(N,Edges,Points);
-% plot_mp(N,MP1,cutoff);
-% Match(MP1,MP2,cutoff);
 
 %% rigid cluster
 global R;
 R=0;
-%IsHinge=IdentifyHinges(N,Edges,MutuallyPairs,-4);
-tic;
 RigidClusters=IdentifyRigidClusters(N,cutoff,MP1,1);
-% toc;
 
 %% output
 disp(R);
@@ -29,7 +23,6 @@ RigidSet=GetRigidSet(N,RigidClusters);
 
 %% find missing clusters
 RigidSet=OptimizeREP(N,RigidClusters,RigidSet,MP1,cutoff);
-toc;
 disp(R);
 
 for i=1:R
@@ -50,27 +43,10 @@ for i=1:N
 end
 end
 
-function plot_mp(N,MP1,cutoff)
-Y=zeros(1,N*N);
-for i=1:N
-    for j=1:N
-        Y((i-1)*N+j)=MP1(i,j);
-    end
-end
 scatter(1:N*N,Y,2);
 hold on;
 disp(cutoff);
 plot([1,N*N],[cutoff,cutoff]);
 xlabel('Vertices pairs number','FontSize',20);
 ylabel('lg(\Delta_i{}_j)','FontSize',20);
-end
-
-%% 判断三点共线
-function OK=pd_commonline(x,y,z)
-a=norm(x-y);
-b=norm(x-z);
-c=norm(y-z);
-p=(a+b+c)/2;
-S=(p*(p-a)*(p-b)*(p-c))^1/2;
-OK=(S<1e-5);
 end
